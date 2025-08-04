4767c8d3ebe676fb1d72c06d34525b408c20595b
Gerardo <g.socias29@gmail.com>
2025-08-04 22:04:33 +0800

    updated docker and eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -33,6 +33,10 @@ export default tseslint.config(
         "error",
         { checksVoidReturn: { attributes: false } },
       ],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
     },
   },
   {
